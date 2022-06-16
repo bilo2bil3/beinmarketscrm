e9b91d1ae448fdf0f8b0f5db0c5f69de2a2dd5a4
abdallaayman21 <abdallaayman190@gmail.com>
2022-06-16 22:27:05 +0300

    Fix list all agents if superuser & notitfication websocket

--- djcrm/asgi.py
+++ djcrm/asgi.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 import os
 import django
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 
 from django.core.asgi import get_asgi_application
 
@@ -21,9 +22,11 @@ from schedule.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({ 
     "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                websocket_urlpatterns
+            )
         )
     )
 })
