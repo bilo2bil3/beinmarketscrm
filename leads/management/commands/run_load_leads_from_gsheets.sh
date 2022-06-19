8ae5068c9085cca14f34a45179727ae0b7503fd1
mazenalhabeby <a.mazen.dessouky@gmail.com>
2022-06-19 14:31:43 +0200

    commit

--- leads/management/commands/run_load_leads_from_gsheets.sh
+++ leads/management/commands/run_load_leads_from_gsheets.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd $HOME/beinmarketscrm
 source $HOME/beinmarketscrm/venv/bin/activate
-export GOOGLE_API_KEY=AIzaSyD8lNEjfyY6APNWikfqB6FfyOLfk4pb-M0
+export GOOGLE_API_KEY=AIzaSyD0Q7cZbWWpsgRea3v2TcNEdsbEAoRPZ10
 export READ_DOT_ENV_FILE=True
 python manage.py load_leads_from_gsheets
