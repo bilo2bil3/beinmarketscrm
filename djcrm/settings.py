b7b2504ab79dbb52a53ebf96fb619dca34c3cfa6
Samer98 <63647160+Samer98@users.noreply.github.com>
2023-10-17 16:31:11 +0300

    add static files

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -11,17 +11,14 @@ READ_DOT_ENV_FILE = env.bool('READ_DOT_ENV_FILE', default=False)
 if READ_DOT_ENV_FILE:
     environ.Env.read_env()
 
-# DEBUG = env('DEBUG')
-# SECRET_KEY = env('SECRET_KEY')
-SECRET_KEY = "1234"
-DEBUG = True
+DEBUG = env('DEBUG')
+SECRET_KEY = env('SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 ALLOWED_HOSTS = env("DJANGO_ALLOWED_HOSTS", default="127.0.0.1,localhost").split(",")
-# DEVELOPMENT_MODE = env("DEVELOPMENT_MODE", default=False)
-DEVELOPMENT_MODE= True
+DEVELOPMENT_MODE = env("DEVELOPMENT_MODE", default=False)
 
 # Application definition
 
